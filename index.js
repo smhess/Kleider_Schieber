f34b1cfb73e4e39a1033af923c4bbd4b1e21a462
smhess <57616320+smhess@users.noreply.github.com>
2019-11-13 12:11:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,33 @@
  */
 $(document).ready(function () {
 
+
+    $('body').click(function(evt){   
+        console.log('body geklickt');
+         
+        if(evt.target.id == "genderSelected")
+           return;
+        //For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
+        if($(evt.target).closest('#genderSelected').length)
+           return;             
+        else{
+            $('#genderSelected').hide();
+            $('#mitte').css('filter', 'blur(0px)');
+            $('.gender').css('color', 'var(--grey-tone)');
+        }
+       //Do processing of click event here for every element except with id menu_content
+    });
+
+    $('#searchArticle').on('input',function(e){
+        console.log($(e.target).val().length);
+        console.log($(e.target).val());
+        if ($(e.target).val().length == 0) {
+            console.log('nichts eingegeben')
+        } else {
+            console.log('etwas eingegeben');   
+        }
+    });
+
     $(document).on("click", '#registrierenLogin', function (event) {
         $('#mitte').css('filter', 'blur(2px)');
         $('#LoginRegistrationPlugin').css('display', 'block');
@@ -14,19 +41,30 @@ $(document).ready(function () {
         $('#mitte').css('filter', 'blur(0px)');
     });
 
+    $(document).on("click", '#aboutKleiderSchieberButton', function (event) {
+        $('#mitte').css('display', 'none');
+        $('#AboutKleiderSchieber').css('display', 'block');
+    });
+
     $(document).on('click', '#woman', function(event){
         $('#woman').css('color', 'var(--bg-colorTone)');
-        $('.genderSelected').css('display', 'block');
+        $('#men').css('color', 'var(--grey-tone)');
+        $('#child').css('color', 'var(--grey-tone)');
+        $('#genderSelected').css('display', 'block');
         $('#mitte').css('filter', 'blur(2px)');
     });
     $(document).on('click', '#men', function(event){
         $('#men').css('color', 'var(--bg-colorTone)');
-        $('.genderSelected').css('display', 'block');
+        $('#child').css('color', 'var(--grey-tone)');
+        $('#woman').css('color', 'var(--grey-tone)');
+        $('#genderSelected').css('display', 'block');
         $('#mitte').css('filter', 'blur(2px)');
     });
     $(document).on('click', '#child', function(event){
         $('#child').css('color', 'var(--bg-colorTone)');
-        $('.genderSelected').css('display', 'block');
+        $('#woman').css('color', 'var(--grey-tone)');
+        $('#men').css('color', 'var(--grey-tone)');
+        $('#genderSelected').css('display', 'block');
         $('#mitte').css('filter', 'blur(2px)');
     });
 
