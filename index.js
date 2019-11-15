7554e3a3b85637f6705a1950840555494c25ccee
smhess <57616320+smhess@users.noreply.github.com>
2019-11-15 16:42:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,90 +4,102 @@
 $(document).ready(function () {
 
 
-    $('body').click(function(evt){   
+
+    $('body').click(function (evt) {
         console.log('body geklickt');
-         
-        if(evt.target.id == "genderSelected")
-           return;
+
+        if (evt.target.id == "genderSelected")
+            return;
         //For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
-        if($(evt.target).closest('#genderSelected').length)
-           return;             
-        else{
+        if ($(evt.target).closest('#genderSelected').length)
+            return;
+        else {
             $('#genderSelected').hide();
-            $('#mitte').css('filter', 'blur(0px)');
+            $('#MainPart').css('filter', 'blur(0px)');
             $('.gender').css('color', 'var(--grey-tone)');
         }
-       //Do processing of click event here for every element except with id menu_content
+        //Do processing of click event here for every element except with id menu_content
     });
 
-    $('#searchArticle').on('input',function(e){
+    $('#searchArticle').on('input', function (e) {
         console.log($(e.target).val().length);
         console.log($(e.target).val());
         if ($(e.target).val().length == 0) {
             console.log('nichts eingegeben')
         } else {
-            console.log('etwas eingegeben');   
+            console.log('etwas eingegeben');
         }
     });
 
+    $(document).on('click', '#kleiderSchieber', function(event){
+        $('#mitte').siblings().hide();
+        $('#mitte').show();
+    })
+
     $(document).on("click", '#registrierenLogin', function (event) {
-        $('#mitte').css('filter', 'blur(2px)');
-        $('#LoginRegistrationPlugin').css('display', 'block');
+        $('#MainPart').css('filter', 'blur(2px)');
+        $('#LoginRegistrationPlugin').show();
         $('#LoginRegistrationPlugin').css('filter', 'blur(0px)');
     });
 
-    $(document).on('click', '.CloseRegistrationLogin', function(event){
-        $('#LoginRegistrationPlugin').css('display', 'none');
+    $(document).on('click', '.CloseRegistrationLogin', function (event) {
+        $('#LoginRegistrationPlugin').hide();
         $('#mitte').css('filter', 'blur(0px)');
     });
 
     $(document).on("click", '#aboutKleiderSchieberButton', function (event) {
-        $('#mitte').css('display', 'none');
-        $('#AboutKleiderSchieber').css('display', 'block');
+        $('#AboutKleiderSchieber').siblings().hide();
+        $('#AboutKleiderSchieber').show();
+    });
+    $(document).on("click", '.gender', function (event) {
+        $('#MainPart').css('filter', 'blur(2px)');
     });
 
-    $(document).on('click', '#woman', function(event){
+    $(document).on('click', '#woman', function (event) {
         $('#woman').css('color', 'var(--bg-colorTone)');
         $('#men').css('color', 'var(--grey-tone)');
         $('#child').css('color', 'var(--grey-tone)');
-        $('#genderSelected').css('display', 'block');
-        $('#mitte').css('filter', 'blur(2px)');
+        $('#genderSelected').show();
     });
-    $(document).on('click', '#men', function(event){
+    $(document).on('click', '#men', function (event) {
         $('#men').css('color', 'var(--bg-colorTone)');
         $('#child').css('color', 'var(--grey-tone)');
         $('#woman').css('color', 'var(--grey-tone)');
-        $('#genderSelected').css('display', 'block');
-        $('#mitte').css('filter', 'blur(2px)');
+        $('#genderSelected').show();
     });
-    $(document).on('click', '#child', function(event){
+    $(document).on('click', '#child', function (event) {
         $('#child').css('color', 'var(--bg-colorTone)');
         $('#woman').css('color', 'var(--grey-tone)');
         $('#men').css('color', 'var(--grey-tone)');
-        $('#genderSelected').css('display', 'block');
-        $('#mitte').css('filter', 'blur(2px)');
+        $('#genderSelected').show();
     });
 
-    // var SelectRorL = ['SelectLogin', 'SelectRegistration'];
+    
+    $(document).on('click', '.SelectLR', function (event) {
+        // var test = $(event.target.parentNode.parentNode.parentNode.parentNode).find('#MainPart').find('.LRContainer');
+        $('#LoginRegistrationPlugin').hide();
+        $('.LRContainer').show();
+        $('#mitte').hide();
+        $('#AboutKleiderSchieber').hide();
+        var SelectLR_ID = event.currentTarget.id;
+        console.log(SelectLR_ID);
+        // var SelectRorL = ['LoginSelected', 'RegistrationSelected'];
+        if(SelectLR_ID == 'SelectLogin'){
+            console.log('login');
+            $('#LoginSelected').css('display', 'block');
+            $('#RegistrationSelected').css('display', 'none');
+        }
+        else if(SelectLR_ID == 'SelectRegistration'){
+            console.log('registrieren');
+            $('#LoginSelected').css('display', 'none');
+            $('#RegistrationSelected').css('display', 'block');
+        }
+        else{
+            console.log('fehler'); 
+        }
+    });
 
-    // $(document).on('click', '.SelectLR', function(event){
-
-    //     $('#LoginRegistrationPlugin').css('display', 'none');
-    //     var SelectLR_ID = event.currentTarget.id;
-    //     console.log(SelectLR_ID);
-    //     if(SelectLR_ID = SelectRorL[0]){
-    //         $('Einloggen').appendTo('#heading_login');
-    //         $('Login').appendTo('#login_button');
-    //     }
-    //     else if(SelectLR_ID = SelectRorL[1]){
-    //         $('Registrieren').appendTo('#heading_login');
-    //         $('registrieren').appendTo('#login_button');
-    //     }
-
-
-    // });
-
-    // $(document).on('click', '#SelectLogin', function(event){
+    // $(document).on('click', '#SelectLogin', function (event) {
     //     $('#mitte').css('display', 'none');
     //     $('#container').css('display', 'flex');
     //     $('#LoginSide').css('display', 'block');
@@ -95,6 +107,6 @@ $(document).ready(function () {
 
     // });
 
-  
+
 
 });
