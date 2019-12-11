f7c3c4a7f29bcb207437aaab4078ac696cf42927
christiansas <57615871+christiansas@users.noreply.github.com>
2019-12-11 22:49:12 +0100

    Add files via upload

--- kleider_schieber_projekt.sql
+++ kleider_schieber_projekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 22. Nov 2019 um 23:33
+-- Erstellungszeit: 11. Dez 2019 um 22:45
 -- Server-Version: 10.4.8-MariaDB
 -- PHP-Version: 7.3.11
 
@@ -25,6 +25,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `klamotten_damen`
+--
+
+CREATE TABLE `klamotten_damen` (
+  `id` int(11) NOT NULL,
+  `oberteil` tinyint(1) NOT NULL,
+  `unterteil` tinyint(1) NOT NULL,
+  `schuhe` tinyint(1) NOT NULL,
+  `asse_tasche` tinyint(1) NOT NULL,
+  `beschreibung` varchar(500) COLLATE utf8_bin NOT NULL,
+  `bild` varchar(500) COLLATE utf8_bin NOT NULL,
+  `preis` char(100) COLLATE utf8_bin NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `size` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `klamotten_herren`
+--
+
+CREATE TABLE `klamotten_herren` (
+  `id` int(11) NOT NULL,
+  `oberteil` tinyint(1) NOT NULL,
+  `unterteil` tinyint(1) NOT NULL,
+  `schuhe` tinyint(1) NOT NULL,
+  `asse_tasche` tinyint(1) NOT NULL,
+  `beschreibung` varchar(500) COLLATE utf8_bin NOT NULL,
+  `bild` varchar(500) COLLATE utf8_bin NOT NULL,
+  `preis` char(100) COLLATE utf8_bin NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `size` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `klamotten_kinder`
+--
+
+CREATE TABLE `klamotten_kinder` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `oberteil` tinyint(1) NOT NULL,
+  `unterteil` tinyint(1) NOT NULL,
+  `schuhe` tinyint(1) NOT NULL,
+  `asse_tasche` tinyint(1) NOT NULL,
+  `beschreibung` varchar(500) COLLATE utf8_bin NOT NULL,
+  `bild` varchar(500) COLLATE utf8_bin NOT NULL,
+  `preis` char(100) COLLATE utf8_bin NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `size` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `users`
 --
 
@@ -32,21 +89,36 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL
+  `password` varchar(200) NOT NULL,
+  `beschreibung` varchar(500) NOT NULL,
+  `bild` varchar(500) NOT NULL,
+  `wohnort` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Daten für Tabelle `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(39, 'Aische', 'aische@aol.de', 'aische312'),
-(40, 'Testuser', 'test@gmail.com', 'test123!'),
-(41, '', '', '');
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `klamotten_damen`
+--
+ALTER TABLE `klamotten_damen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `klamotten_damen_ibfk_1` (`id_user`);
+
+--
+-- Indizes für die Tabelle `klamotten_herren`
+--
+ALTER TABLE `klamotten_herren`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indizes für die Tabelle `klamotten_kinder`
+--
+ALTER TABLE `klamotten_kinder`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `klamotten_kinder_ibfk_1` (`id_user`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -60,10 +132,50 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `klamotten_damen`
+--
+ALTER TABLE `klamotten_damen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT für Tabelle `klamotten_herren`
+--
+ALTER TABLE `klamotten_herren`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT für Tabelle `klamotten_kinder`
+--
+ALTER TABLE `klamotten_kinder`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `klamotten_damen`
+--
+ALTER TABLE `klamotten_damen`
+  ADD CONSTRAINT `klamotten_damen_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints der Tabelle `klamotten_herren`
+--
+ALTER TABLE `klamotten_herren`
+  ADD CONSTRAINT `klamotten_herren_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints der Tabelle `klamotten_kinder`
+--
+ALTER TABLE `klamotten_kinder`
+  ADD CONSTRAINT `klamotten_kinder_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
